7d2e2e8c3149c2fc55a79ce2c54245e6ba235b85
samiyamarium <160397803+samiyamarium@users.noreply.github.com>
2025-01-10 02:40:10 +0500

    Update page.tsx

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -14,11 +14,9 @@ const urlFor = (source: SanityImageSource) =>
 
 const options = { next: { revalidate: 30 } };
 
-export default async function PostPage({
-  params,
-}: {
-  params: { slug: string };
-}) {
+type Params = Promise<{ slug: string }>
+export default async function PostPage({ params }: { params: Params }) 
+  const { slug } = await params; 
   const post = await client.fetch<SanityDocument>(POST_QUERY, params, options);
   const postImageUrl = post.image
     ? urlFor(post.image)?.width(550).height(310).url()
